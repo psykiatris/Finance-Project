ddfd2e1f6fd4de5a2c59d3318dba603c63468417
Patrick Palczewski <psykiatris@gmail.com>
2024-02-20 07:05:06 -0800

    Deleted BaseInfo project, adding BaseInfo class to Library. Created getter/setter static properties to be used by all classes.

--- BaseInfo/BaseInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Finance.Shared;
-
-public class BaseInfo
-{
-    public static double Amount;
-    public static double Rate;  // aka APY, or APR
-    public static double Periods;   // Expend year to months
-    public static DateTime StartDate;   // When period begins
-}
---
+++ FinLibraryModern/BaseInfo.cs
@@ -0,0 +1,10 @@
+namespace Finance.Shared;
+
+public class BaseInfo
+{
+    public static DateTime StartDate { get; set; }
+    public static double Balance { get; set; }
+    public static double Apr { get; set; }
+    public static double Term { get; set; }
+
+}
