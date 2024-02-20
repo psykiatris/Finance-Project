23f4213ad2780a8e06c438cfd6e7bda96d0145db
Patrick Palczewski <psykiatris@gmail.com>
2024-02-19 20:18:47 -0800

    Added fidles to BaseInfo, to be taken from Program.cs and put into Calculate, etc. for processing.

--- BaseInfo/BaseInfo.cs
+++ BaseInfo/BaseInfo.cs
@@ -2,4 +2,8 @@
 
 public class BaseInfo
 {
+    public static double Amount;
+    public static double Rate;  // aka APY, or APR
+    public static double Periods;   // Expend year to months
+    public static DateTime StartDate;   // When period begins
 }
