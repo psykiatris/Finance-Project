7913e2ede4b517f3d5a8cb1398d352855bfad330
Patrick Palczewski <psykiatris@gmail.com>
2024-02-21 15:58:26 -0800

    Made changes to BaseInfo class. Set it up the old-school way, until it's understood how to use properties.

--- FinConsoleApp/Program.cs
+++ FinConsoleApp/Program.cs
@@ -2,4 +2,8 @@
 using Finance.Shared;
 
 double pay = Calculate.AmortizeLoan(114000, 5, 360);
-Console.WriteLine(pay);
+Console.WriteLine($"Your estimated payment is: {pay:C}");
+
+BaseInfo patrick = new(150000, 7.375, 360);
+Console.WriteLine($"Payment on {patrick.Balance:C} is {Calculate.AmortizeLoan(patrick.Balance, patrick.Apr, patrick.TermInMonths):C}");
+Console.WriteLine(patrick.Apr);
--- FinLibraryModern/BaseInfo.cs
+++ FinLibraryModern/BaseInfo.cs
@@ -1,10 +1,26 @@
+using System.Reflection;
+
 namespace Finance.Shared;
 
 public class BaseInfo
 {
-    public static DateTime StartDate { get; set; }
-    public static double Balance { get; set; }
-    public static double Apr { get; set; }
-    public static double Term { get; set; }
+    public DateTime StartDate;
+    public double Balance;
+    public double Apr;
+    public double TermInMonths;
+
+    public BaseInfo() {}
+
+    
+    public BaseInfo(double p, double rate,double periods)
+    {
+        StartDate = DateTime.Now;
+        Balance = p;
+        Apr = rate;
+        TermInMonths = periods;
+    }
+    
+
+
 
 }
