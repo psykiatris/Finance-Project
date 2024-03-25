384c088915456aa10844d1f480921cfedff0b6f3
Patrick Palczewski <psykiatris@gmail.com>
2024-03-25 14:04:27 -0700

    Cleaned up console output to show that calculate formula works.

--- FinConsoleApp/Program.cs
+++ FinConsoleApp/Program.cs
@@ -6,9 +6,12 @@ using Finance.Shared;
 //WriteLine("The Main entry for -> Consoles");
  BaseInfo basePatrick = new()
  {
-     Apr = 5.0, Balance = 114000.00, Name = "Patrick", StartDate = new(2024, 4, 1)
+     Apr = 5.0, TermInMonths = 360,Balance = 114000.00, Name = "Patrick", StartDate = new(2024, 4, 1)
 
  };
  WriteLine($"Account created: {basePatrick.CreateDate}");
-WriteLine($"Start date for loan is {basePatrick.StartDate:d} at {basePatrick.Apr:P1} with a balance of {basePatrick.Balance:C}");
-WriteLine($"The amortized payment would be: {Calculate.CalculateAmortozation(basePatrick.Balance,basePatrick.Apr, 360):C}.");
+WriteLine($"With a balance of: {basePatrick.Balance:C} at {basePatrick.Apr}% over a period of {basePatrick.TermInMonths / 12} years,");
+WriteLine($"The payment would be: {Calculate.CalculateAmortozation(basePatrick.Balance,basePatrick.Apr, 360):C}.");
+WriteLine();
+WriteLine($"Account #: {basePatrick.AccountNumber}");
+
