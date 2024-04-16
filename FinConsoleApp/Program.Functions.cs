680dbd5b527b48c84c65aa2158d0a2070c7bc0d2
Patrick Palczewski <psykiatris@gmail.com>
2024-04-16 08:06:56 -0700

    Changed to AnsiConsole type due to warnings from IDE on type collision.

--- FinConsoleApp/Program.Functions.cs
+++ FinConsoleApp/Program.Functions.cs
@@ -1,5 +1,3 @@
-
-
 public partial class Program
 {
     // Where all my functions will go
--- FinConsoleApp/Program.cs
+++ FinConsoleApp/Program.cs
@@ -1,20 +1,9 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-using System.Net.Http.Headers;
-using System.Runtime.CompilerServices;
-using Boyd.Shared;
+﻿using Boyd.Shared;
+using Spectre.Console;  // Type from loggin library?
 
-//WriteLine("The Main entry for -> Consoles");
- BaseInfo basePatrick = new()
- {
-     Apr = 5.0, TermInMonths = 360,Balance = 114000.00, Name = "Patrick", StartDate = new(2024, 4, 1)
-
- };
- WriteLine($"Account created: {basePatrick.CreateDate}");
-WriteLine($"With a balance of: {basePatrick.Balance:C} at {basePatrick.Apr}% over a period of {basePatrick.TermInMonths / 12} years,");
-WriteLine($"The payment would be: {Calculate.CalculateAmortozation(basePatrick.Balance,basePatrick.Apr, 360):C}.");
-WriteLine();
-double pymt =
-    Calculate.CalculateAmortozation(basePatrick.Balance, basePatrick.Apr, 360);
-WriteLine($"Account #: {basePatrick.AccountNumber}");
+BaseInfo basePatrick = new()
+{
+    Apr = 0.05, Balance = 114000, TermInMonths = 360
+};
+AnsiConsole.WriteLine($"APR is {basePatrick.Apr:P1}");
 
